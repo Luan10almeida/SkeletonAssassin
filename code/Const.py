a4d558d9ea7900295768c6c2f7e68f4c682c1f3b
Luan <almeidaluan58@gmail.com>
2025-02-24 18:23:34 -0300

    Indo mais a fundo no trabalho

---
+++ code/Const.py
@@ -0,0 +1,19 @@
+from pygame.examples.grid import WINDOW_HEIGHT
+
+
+# C
+COLOR_WHITE = (255 , 255, 255)
+COLOR_ORANGE = ( 255 , 128, 0)
+
+
+
+# M
+MENU_OPTION = ('NEW GAME',
+               'SCORE',
+               'EXIT')
+
+
+
+# w
+WIN_WIDTH = 1920
+WIN_HEIGHT = 1080
